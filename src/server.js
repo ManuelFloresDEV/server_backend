ddbabfa195bae11d78efe286467c38d15c4b8c84
Manuel Flores <speed_flores24@hotmail.com>
2024-08-16 16:46:21 -0600

    add license

--- src/server.js
+++ src/server.js
@@ -2,10 +2,12 @@ const express = require("express");
 const userRoutes = require("./routes/user.router");
 const loginRouter = require("./routes/login.router");
 const postRouter = require("./routes/post.router");
+const cors = require("cors");
+const helmet = require("helmet");
 
 const app = express();
 
-app.use(express.json());
+app.use(helmet()), app.use(cors()), app.use(express.json());
 app.use("/user", userRoutes);
 app.use("/auth", loginRouter);
 app.use("/posts", postRouter);
